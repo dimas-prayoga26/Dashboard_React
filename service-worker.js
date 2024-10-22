f115b59964f9d9391aff951b627aa17a9eea207d
Teguhriyadi <ilham.teguh55@gmail.com>
2024-10-22 15:44:50 +0700

    Update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a1e102c959bf4badc4be17009c2ed094.js"
+  "/precache-manifest.4d986ccbc8e12e396cf17148b3dda8e9.js"
 );
 
 self.addEventListener('message', (event) => {
