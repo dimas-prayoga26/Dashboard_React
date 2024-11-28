08a8b74f61b3f802bf2fed9b66194aa032c8af8d
Ilham-Teguh23 <mohammadteguhriyadi23@gmail.com>
2024-11-28 11:48:07 +0700

    Modified: Update Dashboard Vercel 28 11 2024

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.38e8deefaaa30c030ea4bb5cafcded72.js"
+  "/precache-manifest.921c5f34243f47f7eae550f00ea8b809.js"
 );
 
 self.addEventListener('message', (event) => {
